a0a64f6a0589ed56f32d9dcb8d596de557420c32
deciliano <diogo_deciliano@hotmail.com>
2024-06-17 17:12:06 -0300

    corrected identification in the product model

--- products/src/product.py
+++ products/src/product.py
@@ -1,9 +1,9 @@
 from datetime import datetime
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
+from db import db
 
 class Product(db.Model):
+        __tablename__ = 'products'
+
         id = db.Column(db.Integer, primary_key=True)
         name = db.Column(db.String(80), nullable=False)
         desc = db.Column(db.String(200), nullable=True)
@@ -12,16 +12,13 @@ class Product(db.Model):
         created_at = db.Column(db.DateTime, default=datetime.now, nullable=False)
         updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now, nullable=False)
 
-def __repr__(self):
-    return f'<Product {self.name}>'
-
-def to_dict(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'desc': self.desc,
-            'price': self.price,
-            'quantity': self.quantity,
-            'created_at': self.created_at,
-            'updated_at': self.updated_at
-        }
+        def to_dict(self):
+                return {
+                'id': self.id,
+                'name': self.name,
+                'desc': self.desc,
+                'price': self.price,
+                'quantity': self.quantity,
+                'created_at': self.created_at,
+                'updated_at': self.updated_at
+                }
