8b2c734034f2c44adefb2dc96d08bb373fc8bcd9
deciliano <diogo_deciliano@hotmail.com>
2024-06-21 23:54:02 -0300

    Corrected the product update route

--- products/src/productController.py
+++ products/src/productController.py
@@ -26,7 +26,7 @@ def get_all_products_route():
     products = product_service.get_all_products()
     return jsonify([product.to_dict() for product in products])
 
-@product_bp.route('/product/update/<int:product_id>,', methods=['PUT'])
+@product_bp.route('/product/update/<int:product_id>', methods=['PUT'])
 def update_product_route(product_id):
     data = request.get_json()
     product = product_service.update_product(product_id, data)
